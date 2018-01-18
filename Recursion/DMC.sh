1e06c6796451e9ee318b796833383a19b670fcd3
remote <remote@remote.com>
2018-01-18 13:04:46 +0000

    Yet another

---
+++ Recursion/DMC.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "-------------------------------------------------"
+echo  -e  "\033[44m  HOSTNAME: \e[0m"
+echo -e "\033[41m `hostname` \e[0m"
+echo "-------------------------------------------------"
+echo "FILE PERMISSIONS"
+ls -l /jboss/app/dac/data/bgc_doctor/rpi/
+
